7aa33b7a26ee132eb9ede95afcf8a61dac163f8b
zhagzinan <798618048@qq.com>
2015-04-09 18:09:37 +0800

    common

--- rest/settings.py
+++ rest/settings.py
@@ -1,5 +1,16 @@
 #coding=utf-8
 SERVER_NAME = '127.0.0.1:5000'
+
+# MONGO_HOST = '127.0.0.1'
+# MONGO_PORT = 45606
+# MONGO_USERNAME = 'user'
+# MONGO_PASSWORD = 'user'
+# MONGO_DBNAME = 'rest'
+# RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+# ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+# sudo service mongo restart
+# mongo
+
 DOMAIN = {
     'people': {
         'name': '张子楠',
