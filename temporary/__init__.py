285504f48cd4c2b52c6e5e5896aa794e55d6bdc6
zhagzinan <798618048@qq.com>
2015-04-09 17:15:20 +0800

    common

--- temporary/__init__.py
+++ temporary/__init__.py
@@ -1,1 +1,9 @@
-__author__ = 'openerp'
+#coding=utf=8
+import os
+import mimetypes
+
+
+path = '/home/openerp/图片/20131111-4（1）.jpg'
+file = open(path, 'rb')
+ctype, encoding = mimetypes.guess_type(path)
+pass
